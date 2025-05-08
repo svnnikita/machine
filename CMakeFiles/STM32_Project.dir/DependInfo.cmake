4b53cad3a72333a32aa2590c9c82d453a2edaae8
svnnikita <svnnikitos@gmail.com>
2025-05-08 12:01:36 +0300

    second commit

---
+++ CMakeFiles/STM32_Project.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nikita/cubemx_projects/machine/Core/Src/main.c" "CMakeFiles/STM32_Project.dir/Core/Src/main.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Core/Src/main.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/STM32_Project.dir/Core/Src/stm32f4xx_hal_msp.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Core/Src/stm32f4xx_hal_msp.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Core/Src/stm32f4xx_it.c" "CMakeFiles/STM32_Project.dir/Core/Src/stm32f4xx_it.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Core/Src/stm32f4xx_it.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Core/Src/system_stm32f4xx.c" "CMakeFiles/STM32_Project.dir/Core/Src/system_stm32f4xx.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Core/Src/system_stm32f4xx.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.o.d"
+  "/home/nikita/cubemx_projects/machine/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.o" "gcc" "CMakeFiles/STM32_Project.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
